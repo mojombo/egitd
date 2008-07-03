52ac44a2142957211c8bf3d7b5548f2d360a2278
Tom Preston-Werner <tom@mojombo.com>
2008-07-02 20:50:48 -0700

    add header comments and license to pipe.erl

--- elibs/pipe.erl
+++ elibs/pipe.erl
@@ -1,3 +1,35 @@
+% pipe.erl
+%
+% This module implements a pipe data structure. This pipe implementation is
+% designed as a fifo for bytes. You write bytes *to* the pipe and then can
+% read those same bytes *from* the pipe. This is useful when dealing with
+% chunked data from an external port. All of the chunked data can be written
+% to the pipe and then you can read specific numbers of bytes from the pipe.
+% This is necessary if you wish to do your own packet length management.
+%
+% (The MIT License)
+% 
+% Copyright (c) 2008 Tom Preston-Werner
+% 
+% Permission is hereby granted, free of charge, to any person obtaining
+% a copy of this software and associated documentation files (the
+% 'Software'), to deal in the Software without restriction, including
+% without limitation the rights to use, copy, modify, merge, publish,
+% distribute, sublicense, and/or sell copies of the Software, and to
+% permit persons to whom the Software is furnished to do so, subject to
+% the following conditions:
+% 
+% The above copyright notice and this permission notice shall be
+% included in all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+% MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+% IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+% CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+% TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+% SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 -module(pipe).
 -export([new/0, write/2, read/2, size/1]).
 
